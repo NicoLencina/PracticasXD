4b697eb429f16b10b24103ad7c6d855e0b5fb8e4
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-06-06 19:13:10 -0300

    1

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,21 @@ let cantidadUser: number = 50;
 let Usuarios: string[] = new Array(cantidadUser);
 let Contrasenas: number[] = new Array(cantidadUser);
 
+let verific:boolean=false;
+
+function encuentrame(user:string[],pass:number[]):boolean{
+  let user2:string;
+  let pass2:number;
+for (let index = 0; index < user.length; index++) {
+  user2=user[index];
+  pass2=pass[index];
+  if (){ 
+
+  }
+  
+}
+}
+
 butt.addEventListener("click", () => {
   let UserIngresado: string = usuario.value;
   let passIngresado: number = password.value;
